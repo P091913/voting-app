e6cf0bf0f180f22f8685b4f16ad3ed66e8f69ce4
willyoder <willyoder5000@gmail.com>
2024-06-18 11:44:13 -0500

    Made a separate components folder for Modals and referenced them in the voting page - they can be used.

--- VotingApp/VotingApp/obj/Debug/net8.0/VotingApp.AssemblyInfo.cs
+++ VotingApp/VotingApp/obj/Debug/net8.0/VotingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VotingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b51983e8e35b83298ea667a47aa0adc7f2af6bef")]
 [assembly: System.Reflection.AssemblyProductAttribute("VotingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VotingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
