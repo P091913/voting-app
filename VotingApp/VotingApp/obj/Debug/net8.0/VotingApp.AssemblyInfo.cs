6f00f731e5826a2a51c06c6bf941dfecaa216a74
SylsWorld <sylvermoones.world@gmail.com>
2024-07-22 10:51:07 -0500

    Got All changes from Test-Main

--- VotingApp/VotingApp/obj/Debug/net8.0/VotingApp.AssemblyInfo.cs
+++ VotingApp/VotingApp/obj/Debug/net8.0/VotingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VotingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71eb9a762ad83bb751094a9d8498977dca35fe5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3134241b34f0cfc6ab0ab9777274a76c9835e97f")]
 [assembly: System.Reflection.AssemblyProductAttribute("VotingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VotingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
