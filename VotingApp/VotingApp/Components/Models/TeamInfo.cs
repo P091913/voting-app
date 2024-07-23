b7a7b0288e09119af3650cbdc7dbb88e3f47f774
SylsWorld <sylvermoones.world@gmail.com>
2024-07-23 10:47:22 -0500

    Renamed Admin page and removed redundant models folder

--- VotingApp/VotingApp/Components/Models/TeamInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VotingApp.Components.Models;
-
-public class TeamInfo
-{
-    public string TeamLevel { get; set; }
-    public string TeamName { get; set; }
-    
-    public decimal VotesSummary { get; set; }
-}
