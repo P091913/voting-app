1134c191cc24deffe8c76e8653e4a267992a0f48
JohnKooo <125311987+JohnKooo@users.noreply.github.com>
2024-07-24 09:55:55 -0500

    Squashed commit of the following:
    commit f310818e223d7b0216f499f9e0854a086f367b8b
    Author: JohnKooo <125311987+JohnKooo@users.noreply.github.com>
    Date:   Mon Jul 22 11:27:21 2024 -0500
    
        Update
    
        Updated migration with new models.
    
    commit dbb40c81607119f7919b98c2c27a2d56480069f2
    Author: JohnKooo <125311987+JohnKooo@users.noreply.github.com>
    Date:   Tue Jul 16 11:26:57 2024 -0500
    
        updated
    
        Updated to the new model and is now having issues making a new migration.

--- VotingApp/VotingApp/Data/ApplicationDBContext.cs
+++ VotingApp/VotingApp/Data/ApplicationDBContext.cs
@@ -6,9 +6,10 @@ namespace VotingApp.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        static readonly string connectionString = "server=10.12.182.86;database=voting_app;user=developer;password=FPDev154;";
+        //static readonly string connectionString = "server=10.12.182.86;database=voting_app;user=developer;password=FPDev154;";
+        static readonly string connectionString = "server=localhost;database=test;user=root;password=1111;";
 
-		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
 			optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
 		}
@@ -23,6 +24,10 @@ namespace VotingApp.Data
         public DbSet<BusinessLogin> BusinessLogins { get; set; }
         public DbSet<General> Voters { get; set; }
         public DbSet<Teams> Teams { get; set; }
+        public DbSet<FinalTeamScores> FinalTeamScores { get; set; }
+        public DbSet<StaffCoach> StaffCoaches { get; set; }
+        public DbSet<TeamData> TeamDatas { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -40,6 +45,16 @@ namespace VotingApp.Data
             .HasOne(a => a.Admin)
             .WithOne(b => b.General)
             .HasForeignKey<Admin>(b => b.Voter_id);
+
+            modelBuilder.Entity<StaffCoach>()
+            .HasOne(a => a.General)
+            .WithOne(b => b.StaffCoach)
+            .HasForeignKey<General>(b => b.Voter_Id);
+
+            modelBuilder.Entity<Teams>()
+            .HasOne(a => a.TeamData)
+            .WithOne(b => b.Teams)
+            .HasForeignKey<TeamData>(b => b.Team_id);
         }
 
         
--- VotingApp/VotingApp/Models/General.cs
+++ VotingApp/VotingApp/Models/General.cs
@@ -5,13 +5,11 @@ namespace VotingApp.Models
     public class General
     {
         public int Id { get; set; }
-        public int VoterId { get; set; }
+        public int Voter_Id { get; set; }
         public string Name { get; set; }
-        public bool VotedForCollege { get; set; }
-        public bool VotedForBytes { get; set; }
-        public bool VotedForBits { get; set; }
         public BusinessLogin BusinessLogin { get; set; }
         public Admin Admin { get; set; }
+        public StaffCoach StaffCoach { get; set; }
 
     }
 }
