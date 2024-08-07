c0c2893f9a8ead07dffc4a68eae13d628d056553
P091913 <jarrettplate@outlook.com>
2024-08-07 10:30:22 -0500

    j

--- VotingApp/VotingApp/obj/Debug/net8.0/VotingApp.AssemblyInfo.cs
+++ VotingApp/VotingApp/obj/Debug/net8.0/VotingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VotingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+931eb5674788a2838c69033a7894285ac29daef6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65cd3912eb322e96aee22589a45c663f3cf15ef3")]
 [assembly: System.Reflection.AssemblyProductAttribute("VotingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VotingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
