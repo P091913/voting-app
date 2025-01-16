5e455e2fb5f80e83ce587d59775ec696ea3fb563
meghan munford <meghan@munford.dev>
2025-01-16 10:10:11 -0600

    updated page 2, installed missing packages, intalled tailwind

--- Frontend/bytejamfrontend/src/App.js
+++ Frontend/bytejamfrontend/src/App.js
@@ -1,12 +1,18 @@
-import React, { useEffect, useState } from 'react';
-
-import AdminForm from './componets/AdminForm';
+import React from "react";
+import AdminForm from "./componets/AdminForm";
+import Navigation from "./components/Navigation"; // Assuming Navigation.js is in the same folder as AdminForm
 
 function App() {
   return (
     <div className="App">
-      <h1>Admin and General Form</h1>
-      <AdminForm />
+      {/* Navigation Bar */}
+      <Navigation />
+
+      {/* Page Content */}
+      <div className="p-4">
+        <h1 className="text-2xl font-bold">Admin and General Form</h1>
+        <AdminForm />
+      </div>
     </div>
   );
 }
