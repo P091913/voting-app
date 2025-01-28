f9d375b8f40699e6b55b757b1968f54703e0b4b8
Jerica <jericahanna04@gmail.com>
2025-01-28 10:17:54 -0600

    The Welcome App Mobile

--- Frontend/bytejamfrontend/src/componets/Mobile/Page1.js
+++ Frontend/bytejamfrontend/src/componets/Mobile/Page1.js
@@ -1,11 +1,27 @@
 // React Mobile Page 1
 
-// const MyComponent() {
-//     return (
-//       <div>
-//         {
+import React from 'react';
+import { View, Text, StyleSheet, onPress } from 'react-native';
 
-//         }
-//       </div>
-//     );
-//   }
+const App = () => {
+    const handleNext = () => {
+        console.log('button clicked');
+    };
+
+    return (
+    <View>
+      <Text>Welcome To Byte Jam!</Text>
+      <Text>This is the voting app for Byte Jam for 2025!</Text>
+      <Text onPress={handleNext}>
+        Next
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
+
