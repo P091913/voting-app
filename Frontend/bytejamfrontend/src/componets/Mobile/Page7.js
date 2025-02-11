60c5fabee75272950b5240770475ad26aa52290e
Jerica <jericahanna04@gmail.com>
2025-02-11 09:16:53 -0600

    fixing page 7 react

--- Frontend/bytejamfrontend/src/componets/Mobile/Page7.js
+++ Frontend/bytejamfrontend/src/componets/Mobile/Page7.js
@@ -1,7 +1,6 @@
 // React Mobile Page 7
 
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 const App = () => {
     const handleNext = () => {
@@ -9,18 +8,16 @@ const App = () => {
     };
 
     return (
-    <View>
-      <Text style={styles.Text1}>Thank you for voting and your input!</Text>
-      <Text style={styles.Text2}>We will start the event soon!</Text>
-      <Text style={styles.Text3}>Thank you to all the staff and employees for making this possible!</Text>
-    <TouchableOpacity onPress={handleNext}>
-        <Text style={styles.Button}>Submit</Text>
-    </TouchableOpacity>
-    </View>
+    <div>
+      <h1 style={styles.Text1}>Thank you for voting and your input!</h1>
+      <p style={styles.Text2}>We will start the event soon!</p>
+      <p style={styles.Text3}>Thank you to all the staff and employees for making this possible!</p>
+    <button onClick={handleNext} style={styles.Button}>Submit</button>
+    </div>
   );
 };
 
-const styles = StyleSheet.create({
+const styles = {
   Text1: {
     fontSize: 20,
     fontWeight: 'bold', 
@@ -37,7 +34,7 @@ const styles = StyleSheet.create({
     fontSize: 15,
     color: 'Red',
   },
-});
+};
 
 export default App;
 
